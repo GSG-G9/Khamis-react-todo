cbe0e88be9bc4d6502ffbeb6bc3b268397524355
Khamis Hesham <khamis1997.kh@gmail.com>
2020-12-31 20:20:20 +0200

    create add component Relates #5

--- src/App.js
+++ src/App.js
@@ -6,35 +6,25 @@ import "./App.css";
 class App extends React.Component {
   state = {
     items: [
-      { id: 1, name: "ahmed", age: 21 },
-      { id: 2, name: "mahmoud", age: 22 },
-      { id: 3, name: "khamis", age: 23 },
+      { id: 1, task: "create react app by npx create-react-app" },
+      { id: 2, task: "create a component folder and some initial files" },
+      { id: 3, task: "create a static list and render her in table" },
     ],
   };
+
+  addItems = (item) => {
+    item.id = Math.random();
+    const items = this.state.items;
+    items.push(item);
+    this.setState({ items });
+  };
+
   render() {
-    const { items } = this.state;
     return (
       <div className="App">
         <h1>Hi Khamis</h1>
-        <AddItem />
+        <AddItem items={this.state.items} addItems={this.addItems} />
         <DeleteItem />
-        <table className="content-table">
-          <tr>
-            <th>Name</th>
-            <th>Age</th>
-          </tr>
-          {items.map((i) => (
-            <tr key={i.id} className="card">
-              <td>{i.name}</td>
-              <td>{i.age}</td>
-            </tr>
-          ))}
-        </table>
-        <form>
-          <input type="text" placeholder="Enter name...." id="name" />
-          <input type="number" placeholder="Enter age...." id="age" />
-          <input type="submit" value="Add" />
-        </form>
       </div>
     );
   }
--- src/component/addItem.js
+++ src/component/addItem.js
@@ -1,14 +1,62 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class AddItem extends React.Component {
+  state = {
+    task: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (e.target.task.value === "") {
+      return false;
+    } else {
+      this.props.addItems(this.state);
+      this.setState({
+        task: "",
+      });
+    }
+  };
+
   render() {
-    return(
-        <div>
-            <h1>Add Items</h1>
-        </div>
-        
-    ) 
+    const { task } = this.state;
+    return (
+      <div>
+        <table className="content-table">
+          <tbody>
+            <tr>
+              <th>All Tasks</th>
+            </tr>
+            {this.props.items.map((i) => (
+              <tr key={i.id} className="card">
+                <td>{i.task}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter task...."
+            id="task"
+            value={task}
+            onChange={this.handleChange}
+          />
+          <input type="submit" value="Add" />
+        </form>
+      </div>
+    );
   }
 }
 
+AddItem.propTypes = {
+  items: PropTypes.array,
+  addItems: PropTypes.func,
+};
 export default AddItem;
