bf13e41fdc792ed254bec25ff85b7043f3105a50
Khamis Hesham <khamis1997.kh@gmail.com>
2020-12-31 21:44:07 +0200

    create Delete component Relates #6

--- src/component/deleteItem.js
+++ src/component/deleteItem.js
@@ -1,13 +1,31 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class DeleteItem extends React.Component {
   render() {
     return (
       <div>
-        <h1>Delete Items</h1>
+        <table className="content-table">
+          <tbody>
+            <tr>
+              <th>All Tasks</th>
+            </tr>
+            {this.props.items.map((i) => (
+              <tr key={i.id} className="card">
+                <td>{i.task}</td>
+                <button onClick={() => this.props.deleteItems(i.id)}>X</button>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
 }
+
+DeleteItem.propTypes = {
+  items: PropTypes.array,
+  deleteItems: PropTypes.func,
+};
 
 export default DeleteItem;
